7482b466270cd7c0fc8a11ba51bc24ba3f1e35fe
Yerassyl Tokenov <erasyl.tokenov.94@mail.ru>
2022-12-20 17:16:53 +0600

    add all three tasks

--- functions.sh
+++ functions.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
 # Put your code here
+
+pow() {
+	arg1=$1
+	arg2=$2
+	echo $((arg1 ** arg2))
+}
+
+shortest() {
+	len=100
+
+	for i in $*; do
+		tmp=${#i}
+	
+		if [ $len -gt $tmp ]; then
+			len=$tmp
+		fi
+	done
+
+	for y in $*; do
+		tmp=${#y}
+
+		if [ $len -eq $tmp ]; then
+			echo $y
+		fi
+	done
+}
+
+print_log(){
+
+	date=$(date '+%Y-%m-%d %H:%M')
+	echo [$date] $@
+
+}
